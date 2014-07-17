c8c03f7ca5f3fd3796f91d3d5bde4fe84c398df2
Fossj117 <fossj117@gmail.com>
2014-07-17 11:39:52 -0700

    Moved unused code (to be used later) to unused.py

--- opinion_mining/unused.py
+++ opinion_mining/unused.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 
+"""
+File that includes various features that have not yet been 
+integrated into the main system, but will likely be of use later. 
+"""
+
+from __future__ import division
 import re
+import csv
 from nltk.corpus import stopwords
 
 
@@ -103,7 +110,77 @@ class NegationSuffixerAdder():
 
 		return neg_tokens
 
+class LiuFeaturizer():
+	"""
+	Class for scoring sentences using Bing Liu's Opinion Lexicon. 
 
+	Source:
+
+	Minqing Hu and Bing Liu. "Mining and Summarizing Customer Reviews." 
+       Proceedings of the ACM SIGKDD International Conference on Knowledge 
+       Discovery and Data Mining (KDD-2004), Aug 22-25, 2004, Seattle, 
+       Washington, USA,
+
+    Download lexicon at: http://www.cs.uic.edu/~liub/FBS/opinion-lexicon-English.rar
+	"""
+
+	PATH_TO_LEXICONS = "/Users/jeff/Zipfian/opinion-mining/data/Lexicons"
+
+	def __init__(self):
+		"""
+		Read in the lexicons. 
+		"""
+
+		pos_path = self.PATH_TO_LEXICONS + "/Liu/positive-words.txt"
+		neg_path = self.PATH_TO_LEXICONS + "/Liu/negative-words.txt"
+
+		self.pos_lex = self.read_lexicon(pos_path)
+		self.neg_lex = self.read_lexicon(neg_path)
+
+
+	def read_lexicon(self, path):
+		'''
+		INPUT: LiuFeaturizer, string (path)
+		OUTPUT: set of strings
+
+		Takes path to Liu lexicon and 
+		returns set containing the full 
+		content of the lexicon. 
+		'''
+
+		start_read = False
+		lexicon = set() # set for quick look-up
+
+		with open(path, 'r') as f: 
+			for line in f: 
+				if start_read:
+					lexicon.add(line.strip())
+				if line.strip() == "":
+					start_read = True
+		return lexicon
+
+	def featurize(self, tokens):
+		'''
+		INPUT: list of strings
+		OUTPUT: 
+
+		Note: tokens should be a list of 
+		lower-case string tokens, possibly
+		including negation markings. 
+		'''
+
+		features = {}
+
+		doc_len = len(tokens)
+		assert doc_len > 0, "Can't featureize document with no tokens." 
+
+		num_pos = sum([1 if tok in self.pos_lex else 0 for tok in tokens])
+		num_neg = sum([1 if tok in self.neg_lex else 0 for tok in tokens])
+
+		features['liu_pos'] = num_pos/doc_len
+		features['liu_neg'] = num_neg/doc_len
+
+		return features
 
 
 
